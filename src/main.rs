52c461d1d9515bb632159d883a2ce8e696a3b088
Daniele D'Orazio <d.dorazio96@gmail.com>
2019-08-04 16:50:12 +0200

    argument parsing and improve naming

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,65 @@
 use std::fs::File;
 use std::io;
 use std::io::{BufWriter, Write};
+use std::path::PathBuf;
 
 use simdnoise::NoiseBuilder;
+use structopt::StructOpt;
+
+/// Generate random terrain-like meshes using various types of noise functions.
+#[derive(StructOpt)]
+pub struct App {
+    /// Output obj file.
+    #[structopt(short, long, parse(from_os_str), default_value = "terrain.obj")]
+    output: PathBuf,
+
+    /// The width of the final terrain.
+    #[structopt(short, long, default_value = "200")]
+    width: usize,
+
+    /// The depth of the final terrain.
+    #[structopt(short, long, default_value = "200")]
+    depth: usize,
+
+    /// The maximum height of the terrain.
+    #[structopt(short, long, default_value = "20")]
+    amplitude: f32,
+}
 
 #[derive(Debug, Clone)]
 pub struct Terrain {
     heights: Vec<f32>,
     width: usize,
-    height: usize,
+    depth: usize,
 }
 
 impl Terrain {
-    pub fn generate(width: usize, height: usize, amplitude: f32) -> Self {
-        let gen = NoiseBuilder::fbm_2d(width, height);
+    pub fn generate(width: usize, depth: usize, amplitude: f32) -> Self {
+        let mut gen = NoiseBuilder::fbm_2d(width, depth);
+        gen.with_octaves(4).with_freq(0.2);
+
         let heights = gen.generate_scaled(0.0, amplitude);
 
         Terrain {
             heights,
             width,
-            height,
+            depth,
         }
     }
 
-    pub fn z_at(&self, x: usize, y: usize) -> f32 {
+    pub fn height_at(&self, x: usize, y: usize) -> f32 {
         self.heights[y * self.width + x]
     }
 
-    pub fn iter_by_row(&self) -> impl Iterator<Item = (usize, usize, f32)> + '_ {
+    pub fn iter_by_depth(&self) -> impl Iterator<Item = (usize, usize, f32)> + '_ {
         self.heights
             .iter()
             .enumerate()
             .map(move |(i, z)| (i / self.width, i % self.width, *z))
     }
 
-    pub fn iter_pos_by_row(&self) -> impl Iterator<Item = (usize, usize)> + '_ {
-        (0..self.height).flat_map(move |y| (0..self.width).map(move |x| (y, x)))
+    pub fn positions_by_depth(&self) -> impl Iterator<Item = (usize, usize)> + '_ {
+        (0..self.depth).flat_map(move |y| (0..self.width).map(move |x| (y, x)))
     }
 
     pub fn index_of(&self, x: usize, y: usize) -> usize {
@@ -46,18 +70,17 @@ impl Terrain {
         self.width
     }
 
-    pub fn height(&self) -> usize {
-        self.height
+    pub fn depth(&self) -> usize {
+        self.depth
     }
 }
 
 fn main() -> io::Result<()> {
-    let grid_size = 200;
-    let amplitude = 20.0;
+    let opt = App::from_args();
 
-    let terrain = Terrain::generate(grid_size, grid_size, amplitude);
+    let terrain = Terrain::generate(opt.width, opt.depth, opt.amplitude);
 
-    let f = File::create("terrain.obj")?;
+    let f = File::create(opt.output)?;
     let mut f = BufWriter::new(f);
     dump(&mut f, &terrain, true)
 }
@@ -65,19 +88,19 @@ fn main() -> io::Result<()> {
 pub fn dump(w: &mut impl Write, terrain: &Terrain, support: bool) -> io::Result<()> {
     writeln!(w, "o terrain")?;
 
-    for (y, x, z) in terrain.iter_by_row() {
+    for (y, x, z) in terrain.iter_by_depth() {
         writeln!(w, "v {} {} {}", x, y, z)?;
     }
 
     if support {
-        for (y, x) in terrain.iter_pos_by_row() {
+        for (y, x) in terrain.positions_by_depth() {
             writeln!(w, "v {} {} 0", x, y)?;
         }
     }
 
-    let height = terrain.height();
+    let depth = terrain.depth();
     let width = terrain.width();
-    for y in 0..height.saturating_sub(1) {
+    for y in 0..depth.saturating_sub(1) {
         for x in 0..width.saturating_sub(1) {
             let i = 1 + terrain.index_of(x, y);
             let j = 1 + terrain.index_of(x, y + 1);
@@ -86,17 +109,17 @@ pub fn dump(w: &mut impl Write, terrain: &Terrain, support: bool) -> io::Result<
     }
 
     if support {
-        let oi = width * height + 1;
+        let oi = width * depth + 1;
         writeln!(
             w,
             "f {} {} {} {}",
             oi,
-            oi + terrain.index_of(0, height - 1),
-            oi + terrain.index_of(width - 1, height - 1),
+            oi + terrain.index_of(0, depth - 1),
+            oi + terrain.index_of(width - 1, depth - 1),
             oi + terrain.index_of(width - 1, 0),
         )?;
 
-        for y in 0..height.saturating_sub(1) {
+        for y in 0..depth.saturating_sub(1) {
             writeln!(
                 w,
                 "f {} {} {} {}",
@@ -129,10 +152,10 @@ pub fn dump(w: &mut impl Write, terrain: &Terrain, support: bool) -> io::Result<
             writeln!(
                 w,
                 "f {} {} {} {}",
-                oi + terrain.index_of(x + 1, height - 1),
-                oi + terrain.index_of(x, height - 1),
-                1 + terrain.index_of(x, height - 1),
-                1 + terrain.index_of(x + 1, height - 1),
+                oi + terrain.index_of(x + 1, depth - 1),
+                oi + terrain.index_of(x, depth - 1),
+                1 + terrain.index_of(x, depth - 1),
+                1 + terrain.index_of(x + 1, depth - 1),
             )?;
         }
     }
